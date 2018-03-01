4675269ab7bf76823b3c3d1c0db32dee62b54fa1
Katya <katherinenoble0@gmail.com>
2018-03-01 10:24:01 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+  var ucsd_ltlng = {lat:32.880, lng:-117.236};
+
+  //Create map object and specify the DOM element for display
+  var map = new google.maps.Map(document.getElementbyID('map'), {
+    center: ucsd_ltlng,
+    zoon: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: ucsd_ltlng,
+    map: map,
+    title: "UCSD"
+  });
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,13 @@ var data = {
 /*
  * GET tasks page.
  */
+exports.addRSVP = function(req,res){
+  var rsvpEmail = req.body.rsvpEmail;
+  console.log(rsvpEmail); //will print in terminal when POST is made
+  data.rsvp.push(rsvpEmail); //add to current data
+  res.send(rsvpEmail);
+}
+
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
